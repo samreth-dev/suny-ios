e61cbb2e11c5c92159b2ea6400a8e2dca052a43e
samreth-dev <samreth.dev@gmail.com>
2022-12-10 15:57:46 -0600

    feat: adds LocalWeather model

---
+++ Suny/Model/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  DummyBackgroundImages.swift
+//  Suny
+//
+//  Created by Samreth Kem on 12/9/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let bottomImages = [
+        "suny-bg-1",
+        "suny-bg-2",
+        "suny-bg-3",
+        "suny-bg-4",
+        "suny-bg-5",
+        "suny-bg-6",
+    ]
+}
---
+++ Suny/Model/LocalWeather.swift
@@ -0,0 +1,20 @@
+//
+//  LocalWeather.swift
+//  Suny
+//
+//  Created by Samreth Kem on 12/10/22.
+//
+
+import Foundation
+
+struct LocalWeather {
+    var icon: String = ""
+    var city: String = ""
+    var condition: String = ""
+    var temperature: String = ""
+    var wind: String = ""
+    var uv: String = ""
+    var humidity: String = ""
+    var cloud: String = ""
+    var time: String = ""
+}
