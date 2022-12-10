ef2d3a5c280e5efdd2824b1224be13824c9cedd3
samreth-dev <samreth.dev@gmail.com>
2022-12-09 18:55:19 -0600

    Initial Commit

---
+++ Suny/Apps/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Suny
+//
+//  Created by Samreth Kem on 12/9/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
---
+++ Suny/Components/WeatherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Suny
+//
+//  Created by Samreth Kem on 12/9/22.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
+
