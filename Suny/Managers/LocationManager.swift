d5280e6ce1746db99432559855b6452a1196c58d
samreth-dev <samreth.dev@gmail.com>
2022-12-09 19:43:15 -0600

    feat: init LocationManager & WeatherManager

---
+++ Suny/Managers/LocationManager.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManager.swift
+//  Suny
+//
+//  Created by Samreth Kem on 12/9/22.
+//
+
+import Foundation
+import CoreLocation
+
+protocol LocationManagerProtocol {
+    
+}
+
+class LocationManager: LocationManagerProtocol {
+    private var clmanager = CLLocationManager()
+}
---
+++ Suny/Managers/WeatherManager.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherManager.swift
+//  Suny
+//
+//  Created by Samreth Kem on 12/9/22.
+//
+
+import Foundation
+import WeatherKit
+
+protocol WeatherManagerProtocol {
+    
+}
+
+class WeatherManager: WeatherManagerProtocol {
+    
+}
