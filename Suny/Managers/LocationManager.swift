6e0a878f6a66967c407aaf3d6232c8bd555b74f9
samreth-dev <samreth.dev@gmail.com>
2022-12-09 20:26:13 -0600

    update: LocationManager & WeatherManager

--- Suny/Managers/LocationManager.swift
+++ Suny/Managers/LocationManager.swift
@@ -9,9 +9,51 @@ import Foundation
 import CoreLocation
 
 protocol LocationManagerProtocol {
-    
+    var location: CLLocation? { get set }
+    func requestLocationAuth()
+    func requestCurrentLocation()
 }
 
-class LocationManager: LocationManagerProtocol {
+class LocationManager: NSObject, LocationManagerProtocol {
+    var location: CLLocation?
     private var clmanager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        self.clmanager.delegate = self
+    }
+    
+    init(location: CLLocation?) {
+        self.location = location
+    }
+    
+    func requestLocationAuth() {
+        clmanager.requestWhenInUseAuthorization()
+    }
+    
+    func requestCurrentLocation() {
+        if clmanager.authorizationStatus == .authorizedWhenInUse || clmanager.authorizationStatus == .authorizedAlways {
+            clmanager.requestLocation()
+        }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.location = locations.first
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        debugPrint(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    debugPrint("hi")
+                }
+            }
+        }
+    }
 }
--- Suny/Managers/WeatherManager.swift
+++ Suny/Managers/WeatherManager.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 import WeatherKit
+import CoreLocation
 
 protocol WeatherManagerProtocol {
-    
+    var weather: Weather? { get set }
+    func fetchWeather(location: CLLocation)
 }
 
 class WeatherManager: WeatherManagerProtocol {
+    private let service = WeatherService.shared
+    var weather: Weather?
     
+    init(weather: Weather?) {
+        self.weather = weather
+    }
+    
+    func fetchWeather(location: CLLocation) {
+        Task {
+            try? await service.weather(for: location)
+        }
+    }
 }
